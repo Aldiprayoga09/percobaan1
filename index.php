438deb6c55cc1ffb3405d270a1d229f57f28604b
Aldi Prayoga <aldipayoga09@gmail.com>
2024-05-12 09:39:36 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <title>WELCOME TO MY WEBSIDE</title>
+    </head>
+    <body>
+         
+        <h1 align="center">MY BIODATA</h1>
+        <body style="background-color:red"></body>
+        <table border="1" cellspacing="0" cellpadding="5" align="center" width="800">
+            <tr align="center" bgcolor="grenn">
+               <td width="200">DATA DIRI</td>
+               <td width="400">KETERANGAN</td>
+               <td width="200">FOTO</td>
+            </tr>
+            <tr bgcolor="pink ">
+                <td>Nama</td>
+                <td>Aldi prayoga</td>
+                <td rowspan="8"><img src="aldi.jpg" width="200"></td>
+            </tr>
+            <tr bgcolor="pink">
+                <td>Nim</td>
+                <td>2102050736</td>
+            </tr>
+            <tr bgcolor="pink">
+                <td>tempat/tangal lahir</td>
+                <td>rantau durian,09 oktober 2002</td>
+            </tr>
+            <tr bgcolor="pink">
+                <td>Alamat</td>
+                <td>rantau durian 1 kec.lempuing jaya kab.ogan komering ilir prov,sumatra selatan</td>
+            </tr>
+            <tr bgcolor="pink">
+                <td>Agama</td>
+                <td>islam</td>
+            </tr>
+            <tr bgcolor="pink">
+                <td>jenis kelamin</td>
+                <td>laki-laki</td>
+            </tr>
+            <tr bgcolor="pink">>
+                <td>status</td>
+                <td>mahasiswa</td>
+            </tr>
+            <tr bgcolor="pink">
+                <td>hobi</td>
+                <td>olahraga</td>
+            </tr>
+        </table>
+        <h2 align="center">FAKULTAS TEKNOLOGI INFORMASI</h2>
+        <h2 align="center">prodi sistem informasi</h2>
+        <h3 align="center">sistem informasi kelas A</h3>
+    </body>
+</html>
